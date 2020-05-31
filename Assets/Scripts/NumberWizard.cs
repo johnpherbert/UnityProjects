1a8c88233cc4e605c89af185ba3d3106ef91c466
John Herbert <john.p.herbert@gmail.com>
2020-05-31 11:11:47 -0400

    Inital NumberWizard Files

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block : MonoBehaviour
+public class NumberWizard : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("yolo");
+        Debug.Log("Welcome to number wizard, yo");
+
     }
 
     // Update is called once per frame
