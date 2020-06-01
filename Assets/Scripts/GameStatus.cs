6b34c877a770ca40b99475bb4f1d1d2f3b49c24e
John Herbert <john.p.herbert@gmail.com>
2020-05-31 20:15:07 -0400

    Updated to section 67

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Time.timeScale = gameSpeed;
     }
 }
